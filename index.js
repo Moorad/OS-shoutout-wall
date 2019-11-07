ebe0b4e8fc7a09fbf65849b748016b52b9f361c6
Moorad <moorad2003@gmail.com>
2019-11-07 13:18:03 +0000

    rearranged the files

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const app = express();
 
 app.listen(process.env.PORT || 4000, () => {
 	console.log('Server is running | port: '+(process.env.PORT || 4000));
+});
+
+app.get('/', (req,res) => {
+	console.log(req);
 })
