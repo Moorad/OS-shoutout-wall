1fb0e790ba0498f3fc30d5cb3c4b98b9886479fb
MooradAltamimi <moorad2003@gmail.com>
2019-11-20 22:30:11 +0000

    Developed the landing page

--- index.js
+++ index.js
@@ -11,16 +11,23 @@ let fetchedUsers = [];
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('public'));
 
 app.listen(process.env.PORT || 4000, () => {
 	console.log('RUNNING :: Port ' + (process.env.PORT || 4000));
 });
 
-app.get('/', (req, res) => {
-	console.log('PING');
-	res.send('PING');
-	res.end();
-})
+// app.get('/', (req, res) => {
+// 	res.sendFile(__dirname+'/public/index.html')
+// 	// console.log('PING');
+// 	// res.send('PING');
+// 	// res.end();
+// });
+
+
+app.get('/setup', (req,res) => {
+	res.sendFile(__dirname+'/public/routes/setup/index.html');
+});
 
 app.get('/push_user', (req, res) => {
 	let userId = req.query.uid;
@@ -98,6 +105,12 @@ app.get('/log', (req, res) => {
 	let data = JSON.parse(req.query.data);
 	console.log(data)
 })
+
+app.use(function(req, res, next){
+	res.status(404);
+	res.sendFile(__dirname+'/public/routes/404/index.html');
+  });
+
 
 function getQueryVariable(query, variable) {
 	var vars = query.split('&');
